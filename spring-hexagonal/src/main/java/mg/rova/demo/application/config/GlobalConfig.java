af2291650727638f98d4849350b95919a69869e9
Utilisateur7Ext2 <utilisateur7.c2i.ext@orange.com>
2024-01-19 16:09:18 +0400

    fix: change pom.xml for spring-hexagonal

--- spring-hexagonal/src/main/java/mg/rova/demo/application/config/GlobalConfig.java
+++ spring-hexagonal/src/main/java/mg/rova/demo/application/config/GlobalConfig.java
@@ -3,6 +3,7 @@ package mg.rova.demo.application.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties
 @EnableJpaRepositories(basePackages = "mg.rova.demo.infrastructure.adapters.secondary.repository")
-// @EnableElasticsearchRepositories("mg.rova.demo.infrastructure.adapters.secondary.elasticsearch")
+@EnableElasticsearchRepositories("mg.rova.demo.infrastructure.adapters.secondary.elasticsearch")
 public class GlobalConfig {
 }
